4e02d1118e9cc5827bf5cb4c562b1eed56739cd7
dzmitry_kavalenka <dzmitry_kavalenka@epam.com>
2021-07-30 09:34:23 +0300

    - Started

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,11 +29,11 @@ publishing {
 
 dependencies {
     implementation(kotlin("stdlib", version = "${project.properties["kotlinVersion"]}"))
-    implementation("io.appium:java-client:7.5.1")
     implementation("org.dk.selenk:common:1.0-SNAPSHOT")
+    implementation("io.appium:java-client:7.5.1")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.1.0")
-    testImplementation("io.mockk:mockk:1.11.0")
     testImplementation("org.hamcrest:hamcrest-all:1.3")
+    testImplementation("io.mockk:mockk:1.11.0")
 }
 
 tasks {
