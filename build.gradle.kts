fd41cd32df886694d7004a78189f66135f29fecb
Dzmitry Kavalenka <dzmitry_kavalenka@epam.com>
2022-03-12 12:41:01 -0600

    - Updated gradle properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,9 +31,9 @@ dependencies {
     implementation(kotlin("stdlib", version = "${project.properties["kotlinVersion"]}"))
     implementation("org.dk.selenk:common:1.0-SNAPSHOT")
     implementation("io.appium:java-client:7.6.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.hamcrest:hamcrest-all:1.3")
-    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("io.mockk:mockk:1.12.3")
 }
 
 tasks {
